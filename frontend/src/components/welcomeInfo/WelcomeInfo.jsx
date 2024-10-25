f2f90a0bb62e13d5cc99165b1135ce274aff248b
farahbed <farah.bendriss.la@gmail.com>
2024-10-25 12:22:13 +0200

    same for WelcomeInfo.jsx +css

--- frontend/src/components/welcomeInfo/WelcomeInfo.jsx
+++ frontend/src/components/welcomeInfo/WelcomeInfo.jsx
@@ -1,21 +1,14 @@
+// src/components/WelcomeInfo.jsx
+
 import React from 'react';
 import './WelcomeInfo.css';
 
-const WelcomeInfo = ({ userProfile }) => {
-  return (
-    <section className="welcome">
-      {userProfile ? ( // Vérifiez que userProfile n'est pas null
-        <>
-          <h2>Welcome back</h2>
-          <h1>{userProfile.name}!</h1>
-          <button>Edit Name</button>
-        </>
-      ) : (
-        <h2>Loading...</h2> 
-      )}
-    </section>
-  );
-};
+const WelcomeInfo = ({ firstName, lastName }) => (
+  <div className="welcome-info">
+    <h1 className="welcome-message">Bienvenue, {firstName} {lastName} !</h1>
+    <p className="welcome-description">Voici un résumé de vos informations bancaires.</p>
+  </div>
+);
 
 export default WelcomeInfo;
 
