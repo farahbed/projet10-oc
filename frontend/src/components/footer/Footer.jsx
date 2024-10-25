a55122f871a1902769755882697e144bf9817206
farahbed <farah.bendriss.la@gmail.com>
2024-10-25 12:19:25 +0200

    loste footer did it again footer.jsx + css

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+// src/components/Footer.jsx
+
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    <p className="footer-text">Copyright © 2024 Bank. Tous droits réservés.</p>
+  </footer>
+);
+
+export default Footer;
